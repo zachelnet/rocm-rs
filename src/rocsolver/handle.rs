e525a9b92b5ddd33314769017f51af4726b42c81
radu <radu0071@gmail.com>
2025-05-04 19:35:04 +0300

    undo rocsolver

--- src/rocsolver/handle.rs
+++
@@ -1,6 +0,0 @@
-// src/rocsolver/handle.rs
-
-// RocSOLVER doesn't have its own handle type - it uses the rocBLAS handle
-// So we'll just re-export the rocBLAS handle
-
-pub use crate::rocblas::handle::Handle;
